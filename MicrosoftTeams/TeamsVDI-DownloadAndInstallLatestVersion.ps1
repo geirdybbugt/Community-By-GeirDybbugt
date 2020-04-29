5b545c3fa1b7ff7944d76550ef33cbbab2af88c0
Geir Dybbugt <59667456+geirdybbugt@users.noreply.github.com>
2020-04-29 13:57:49 +0200

    Create TeamsVDI-DownloadAndInstallLatestVersion.ps1

---
+++ MicrosoftTeams/TeamsVDI-DownloadAndInstallLatestVersion.ps1
@@ -0,0 +1,31 @@
+﻿####------------------------------------------------------------------------####
+#### Downloads and install Microsoft Teams x64 for VDI
+#### Can be run later to update packages
+#### 
+#### Creator info: Geir Dybbugt - https://dybbugt.no
+####------------------------------------------------------------------------####
+
+#Variables
+$Masterdestination = "$env:APPDATA\DybbugtNO-Files"
+$TeamsDestination = "$Masterdestination\Teams"
+
+#Folder Structure
+
+    #Creating root folder
+    MD $Masterdestination -force
+
+    #Creating Subfolders
+    MD $TeamsDestination -force
+
+ #Downloading source file
+    $TeamsDownload = ((Invoke-WebRequest -Uri 'https://docs.microsoft.com/en-us/microsoftteams/teams-for-vdi' -UseBasicParsing ).Links | where outerHTML -Like "*64-bit version*").href
+    Start-BitsTransfer -Source $TeamsDownload -Destination "$TeamsDestination"
+
+ #start installation
+    cd $TeamsDestination
+    msiexec /i "$TeamsDestination\teams_windows_x64.msi" ALLUSER=1 ALLUSERS=1
+ 
+  #Cleaning up downloaded files
+    start-sleep -Seconds 10
+    remove-item $TeamsDestination -recurse -Force
+    remove-item $Masterdestination -recurse -Force
