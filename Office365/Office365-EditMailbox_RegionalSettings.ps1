1a5986efaf526db1128de412aec1f5828059217c
Geir Dybbugt <59667456+geirdybbugt@users.noreply.github.com>
2020-09-09 15:17:16 +0200

    update
    update

---
+++ Office365/Office365-EditMailbox_RegionalSettings.ps1
@@ -0,0 +1,18 @@
+﻿##Edit mailbox regional settings
+
+#Connect to Exchange online
+Connect-ExchangeOnline
+
+#List all mailboxes
+get-mailbox
+
+#Check regional configuration for wanted mailbox
+get-MailboxRegionalConfiguration -Identity somemailboxname
+
+ 
+#Set regional configuration for wanted mailbox
+Set-MailboxRegionalConfiguration -Identity somemailboxname -TimeZone "W. Europe Standard Time" -DateFormat "dd.MM.yyyy" -TimeFormat "HH:mm" -Confirm:$true -Language nb-NO
+
+
+#Disconnect to Exchange online
+disconnect-ExchangeOnline
---
+++ Office365/Office365-EnableModernAuthOutlook.ps1
@@ -0,0 +1,19 @@
+﻿# Enable or disable modern authentication for Outlook in Exchange Online
+        # Source https://docs.microsoft.com/en-us/exchange/clients-and-mobile-in-exchange-online/enable-or-disable-modern-authentication-in-exchange-online
+
+# Connect to Exchange Online
+    $UserCredential = Get-Credential
+    $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+    Import-PSSession $Session -DisableNameChecking
+
+# Enable modern auth
+    Set-OrganizationConfig -OAuth2ClientProfileEnabled $true
+
+# Disable modern auth
+    #Set-OrganizationConfig -OAuth2ClientProfileEnabled $false
+
+# Verify status
+    Get-OrganizationConfig | Format-Table Name,OAuth* -Auto
+
+# End session
+    Remove-PSSession $Session
