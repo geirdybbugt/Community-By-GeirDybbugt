a0cb2af128d06d118e9d54acc57c696d92d7caad
Geir Dybbugt <geir@dybbugt.no>
2021-06-29 14:41:39 +0200

    2 new scripts, ex and teams
    Dump new scripts. One for Teams firewall rules and intnue + one to check members of dynamic distribution lists in exonline

---
+++ Office365/Office 365-ExchangeOnline_DynamicDistributionListMembers.ps1
@@ -0,0 +1,64 @@
+﻿####------------------------------------------------------------------------####
+#### Gets information of who is member of dynamic distribution lists
+#### Exports to csv file for use elsewhere
+#### You need to log in to ExOnline with valid credentials
+#### 
+#### Creator info: Geir Dybbugt - https://dybbugt.no
+####------------------------------------------------------------------------####
+
+# set powershell to use tls 1.2
+	[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
+
+# Check if Exchange Online module is installed
+    if (Get-Module -ListAvailable -Name ExchangeOnlineManagement) {
+    Write-Host "Module exists, continuing" -ForegroundColor Green
+    } 
+    else {
+        Write-Host "Module does not exist, starting install" -ForegroundColor Red
+        Install-Module -Name ExchangeOnlineManagement
+    }
+
+
+# Log in to Exchange Online
+    Connect-ExchangeOnline
+
+# Location to save exported CSV
+    $DesktopPath = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::Desktop)
+
+# Get running date
+    $date = Get-Date -Format "dd.MM.yyyy"
+
+# To get the dynamic lists in the tenant
+    $dynamiclists = Get-DynamicDistributionGroup "*" 
+
+# For table later
+    $Table = @()
+
+    $Record = @{
+        "Date" = ""
+        "Group Name" = ""
+        "Name" = ""
+    }
+
+    # Get the member data
+    ForEach($dynamiclist in $dynamiclists){
+    $members = Get-Recipient -RecipientPreviewFilter $dynamiclist.RecipientFilter -OrganizationalUnit $dynamiclist.RecipientContainer | Select name
+        foreach ($member in $members){
+        $record."Date" = $date
+        $record."Group Name" = $dynamiclist
+        $record."Name" = $member.name        
+        $objRecord = New-Object PSObject -property $Record
+        $Table += $objrecord
+        }
+    }
+
+# Show result in console
+    $Table | Out-Host
+    Write-host "Press any key to complete export to file and exit" -ForegroundColor Cyan
+    pause
+
+# Export to CSV file
+    $Table | Export-Csv -NoTypeInformation $DesktopPath\DDL-Members-$date.csv
+
+# Disconnect from Exchange Online
+    Disconnect-ExchangeOnline
