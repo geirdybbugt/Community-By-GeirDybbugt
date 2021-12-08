57da6549252770aecf3b4b0e0ca6cb2626f123c6
Geir Dybbugt <59667456+geirdybbugt@users.noreply.github.com>
2021-12-08 19:54:28 +0100

    Create Win10_OS_disable_IPv6_All_NetCards.ps1

---
+++ Win10/Win10_OS_disable_IPv6_All_NetCards.ps1
@@ -0,0 +1,14 @@
+﻿ <# Script to disable IPv6 on all ethernet cards on device. Intended to run with intune.  
+ IPv6 causes issues with DNS via VPN in some scenarios, hence the script. 
+ 
+ Edited: 08.12.21 - @Geir Dybbugt - Dybbugt.no 
+ #>
+ 
+ # Get a list of all the network cards on the device and save to array
+ $netcards = @(Get-NetAdapterBinding -ComponentID ms_tcpip6 |Select-object -ExpandProperty name)
+
+ # Disable IPv6 on all cards in the list
+ Foreach ($netcard in $netcards)
+    {
+     Disable-NetAdapterBinding -Name "$netcard" -ComponentID ms_tcpip6
+    }
