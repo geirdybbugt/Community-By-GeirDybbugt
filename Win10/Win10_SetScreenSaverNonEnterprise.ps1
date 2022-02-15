c21e2367ae0db05cb6b5b204c8f4d3f3ce9f6bd5
Geir Dybbugt <59667456+geirdybbugt@users.noreply.github.com>
2022-02-15 21:17:01 +0100

    Update Win10_SetScreenSaverNonEnterprise.ps1

--- Win10/Win10_SetScreenSaverNonEnterprise.ps1
+++ Win10/Win10_SetScreenSaverNonEnterprise.ps1
@@ -17,31 +17,19 @@ If ($ENV:PROCESSOR_ARCHITEW6432 -eq "AMD64") {
 }
 
 # Variables
-    $RegPath = "HKCU:\Software\Policies\Microsoft\Windows\control panel\desktop" # Change to HKCU to set for current user only
-    $RegPath2 = "HKCU:\Control Panel\Desktop" 
-    $ScreenSaverName = "scrnsave.scr"
     $ScreenSaverTimeout = "900"
+    $ScreenSaverName ="scrnsave.scr"
 
-# Set values current user admin rights
-if((Test-Path -LiteralPath $RegPath) -ne $true) {
-    New-Item $RegPath -force
-        if ($?) {
-            New-ItemProperty -LiteralPath "$RegPath2" -Name 'ScreenSaveActive' -Value '1'-PropertyType String -Force -ea SilentlyContinue;
-            New-ItemProperty -LiteralPath "$RegPath2" -Name 'ScreenSaverIsSecure' -Value '1' -PropertyType String -Force -ea SilentlyContinue; 
-            New-ItemProperty -LiteralPath "$RegPath2" -Name 'SCRNSAVE.EXE' -Value $ScreenSaverName -PropertyType String -Force -ea SilentlyContinue;
-            New-ItemProperty -LiteralPath "$RegPath2" -Name 'ScreenSaveTimeOut' -Value $ScreenSaverTimeout -PropertyType String -Force -ea SilentlyContinue;
-        } else {
-            write-host "failed" -ForegroundColor Red
-            exit
-            }
-    } else { 
-            New-ItemProperty -LiteralPath "$RegPath2" -Name 'ScreenSaveActive' -Value '1'-PropertyType String -Force -ea SilentlyContinue;
-            New-ItemProperty -LiteralPath "$RegPath2" -Name 'ScreenSaverIsSecure' -Value '1' -PropertyType String -Force -ea SilentlyContinue; 
-            New-ItemProperty -LiteralPath "$RegPath2" -Name 'SCRNSAVE.EXE' -Value $ScreenSaverName -PropertyType String -Force -ea SilentlyContinue;
-            New-ItemProperty -LiteralPath "$RegPath2" -Name 'ScreenSaveTimeOut' -Value $ScreenSaverTimeout -PropertyType String -Force -ea SilentlyContinue;
-    }
+    $RegPath = "HKCU:\Software\Policies\Microsoft\Windows\control panel\desktop" # Change to HKLM to set for the device
+    $RegPath2 = "HKCU:\Control Panel\Desktop" 
+    $RegPath3 = "Software\Policies\Microsoft\Windows\control panel\desktop"
 
-# Set values current user standard rights
+    $GetLastLoggedOnUser = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI"
+    $LastLoggedOnUserSID = Get-ItemProperty -Path $GetLastLoggedOnUser
+    $LastLoggedOnUserSID = $LastLoggedOnUserSID.LastLoggedOnUserSID
+
+
+# Set values for current user with admin rights
 if((Test-Path -LiteralPath $RegPath) -ne $true) {
     New-Item $RegPath -force
         if ($?) {
@@ -51,7 +39,6 @@ if((Test-Path -LiteralPath $RegPath) -ne $true) {
             New-ItemProperty -LiteralPath "$RegPath" -Name 'ScreenSaveTimeOut' -Value $ScreenSaverTimeout -PropertyType String -Force -ea SilentlyContinue;
         } else {
             write-host "failed" -ForegroundColor Red
-            exit
             }
     } else { 
             New-ItemProperty -LiteralPath "$RegPath" -Name 'ScreenSaveActive' -Value '1'-PropertyType String -Force -ea SilentlyContinue;
@@ -59,6 +46,55 @@ if((Test-Path -LiteralPath $RegPath) -ne $true) {
             New-ItemProperty -LiteralPath "$RegPath" -Name 'SCRNSAVE.EXE' -Value $ScreenSaverName -PropertyType String -Force -ea SilentlyContinue;
             New-ItemProperty -LiteralPath "$RegPath" -Name 'ScreenSaveTimeOut' -Value $ScreenSaverTimeout -PropertyType String -Force -ea SilentlyContinue;
     }
+
+# Set values for current user with standard rights
+if((Test-Path -LiteralPath $RegPat2h) -ne $true) {
+    New-Item $RegPath2 -force
+        if ($?) {
+            New-ItemProperty -LiteralPath "$RegPath2" -Name 'ScreenSaveActive' -Value '1'-PropertyType String -Force -ea SilentlyContinue;
+            New-ItemProperty -LiteralPath "$RegPath2" -Name 'ScreenSaverIsSecure' -Value '1' -PropertyType String -Force -ea SilentlyContinue; 
+            New-ItemProperty -LiteralPath "$RegPath2" -Name 'SCRNSAVE.EXE' -Value $ScreenSaverName -PropertyType String -Force -ea SilentlyContinue;
+            New-ItemProperty -LiteralPath "$RegPath2" -Name 'ScreenSaveTimeOut' -Value $ScreenSaverTimeout -PropertyType String -Force -ea SilentlyContinue;
+        } else {
+            write-host "failed" -ForegroundColor Red
+            }
+    } else { 
+            New-ItemProperty -LiteralPath "$RegPath2" -Name 'ScreenSaveActive' -Value '1'-PropertyType String -Force -ea SilentlyContinue;
+            New-ItemProperty -LiteralPath "$RegPath2" -Name 'ScreenSaverIsSecure' -Value '1' -PropertyType String -Force -ea SilentlyContinue; 
+            New-ItemProperty -LiteralPath "$RegPath2" -Name 'SCRNSAVE.EXE' -Value $ScreenSaverName -PropertyType String -Force -ea SilentlyContinue;
+            New-ItemProperty -LiteralPath "$RegPath2" -Name 'ScreenSaveTimeOut' -Value $ScreenSaverTimeout -PropertyType String -Force -ea SilentlyContinue;
+    }
+
+# Set values via HKU as System user - to last logged on user on device
+    # Variables
+        $HKUpath = "HKU:\$LastLoggedOnUserSID\$Regpath3"
+        $HKUpath
+
+    # Map HKU in registry as available drive in OS
+        New-PSDrive -PSProvider Registry -Name HKU -Root HKEY_USERS
+
+        # Liste available PS drives
+            #Get-PSDrive
+
+if((Test-Path -LiteralPath $HKUpath) -ne $true) {
+    New-Item $HKUpath -force
+        if ($?) {
+            New-ItemProperty -LiteralPath "$HKUpath" -Name 'ScreenSaveActive' -Value '1'-PropertyType String -Force -ea SilentlyContinue;
+            New-ItemProperty -LiteralPath "$HKUpath" -Name 'ScreenSaverIsSecure' -Value '1' -PropertyType String -Force -ea SilentlyContinue; 
+            New-ItemProperty -LiteralPath "$HKUpath" -Name 'SCRNSAVE.EXE' -Value $ScreenSaverName -PropertyType String -Force -ea SilentlyContinue;
+            New-ItemProperty -LiteralPath "$HKUpath" -Name 'ScreenSaveTimeOut' -Value $ScreenSaverTimeout -PropertyType String -Force -ea SilentlyContinue;
+        } else {
+            write-host "failed" -ForegroundColor Red
+            }
+    } else { 
+            New-ItemProperty -LiteralPath "$HKUpath" -Name 'ScreenSaveActive' -Value '1'-PropertyType String -Force -ea SilentlyContinue;
+            New-ItemProperty -LiteralPath "$HKUpath" -Name 'ScreenSaverIsSecure' -Value '1' -PropertyType String -Force -ea SilentlyContinue; 
+            New-ItemProperty -LiteralPath "$HKUpath" -Name 'SCRNSAVE.EXE' -Value $ScreenSaverName -PropertyType String -Force -ea SilentlyContinue;
+            New-ItemProperty -LiteralPath "$HKUpath" -Name 'ScreenSaveTimeOut' -Value $ScreenSaverTimeout -PropertyType String -Force -ea SilentlyContinue;
+    }
+
+    # Unmap HKU from available PS drives before exiting
+            Remove-PSDrive hku
 
 # Clears the error log from powershell before exiting
     $error.clear()
