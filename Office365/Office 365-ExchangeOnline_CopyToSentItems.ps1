a00c2e501470bd661c6a81c0fc93c7a5ebd6e9b4
Geir Dybbugt <59667456+geirdybbugt@users.noreply.github.com>
2022-05-06 14:12:25 +0200

    Create Office 365-ExchangeOnline_CopyToSentItems.ps1

---
+++ Office365/Office 365-ExchangeOnline_CopyToSentItems.ps1
@@ -0,0 +1,19 @@
+﻿<# setting to save sent items in the mailbox sent items folder
+When users are using the "send on behalf of or send as functions.
+
+Modified: 06.05.2022 - Geir Dybbugt
+#>
+
+# Info: https://docs.microsoft.com/en-us/exchange/troubleshoot/user-and-shared-mailboxes/sent-mail-is-not-saved
+
+# Connect to exchange online
+    Connect-ExchangeOnline
+
+# Using Exchange PowerShell, for emails Sent As the shared mailbox
+    set-mailbox email@domain.com -MessageCopyForSentAsEnabled $True
+
+# Using Exchange PowerShell, for emails Sent On Behalf of the shared mailbox,
+    set-mailbox email@domain.com -MessageCopyForSendOnBehalfEnabled $True
+
+# Check status
+    get-mailbox -Identity email@domain.com | Select-Object DisplayName,MessageCopyForSendOnBehalfEnabled, MessageCopyForSentAsEnabled | fl
