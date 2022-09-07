94f53285ad6b1c47f6123712cc8660102d5ea636
Geir Dybbugt <59667456+geirdybbugt@users.noreply.github.com>
2022-09-07 13:42:21 +0200

    Update Win10-SetWallpaperAndLockscreenFromUri_CUstomMod.ps1

--- Win10/Win10-SetWallpaperAndLockscreenFromUri_CUstomMod.ps1
+++ Win10/Win10-SetWallpaperAndLockscreenFromUri_CUstomMod.ps1
@@ -17,59 +17,126 @@
     $LockScreenDestinationFile = "$ImageDestinationFolder\LockScreen.png" # Change to your fitting - this is the Lockscreen image
 
 # Creates the destination folder on the target computer
-    md $ImageDestinationFolder -erroraction silentlycontinue
+    IF(!(Test-Path $ImageDestinationFolder)){
+        md $ImageDestinationFolder -erroraction silentlycontinue
+        }
 
-# Downloads the image file from the source location - C:\Users\Public\Pictures\
+# Downloads the image file(s) from the source location(s)
     Start-BitsTransfer -Source $WallpaperURL -Destination "$WallpaperDestinationFile"
     Start-BitsTransfer -Source $LockscreenUrl -Destination "$LockScreenDestinationFile"
 
-# Variables
-    # To set the LockScreen - cannot be changed by the user
-        $RegKeyPath = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP'
+# Registry location variables
+    # General
+        $RegPathDesktopWallpaper = "Control Panel\Desktop"
+
+    # Get info about the last logged on user
+        $GetLastLoggedOnUser = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI"
+        $LastLoggedOnUserSID = Get-ItemProperty -Path $GetLastLoggedOnUser
+        $LastLoggedOnUserSID = $LastLoggedOnUserSID.LastLoggedOnUserSID
+
+    # To set the wallpaper and lockscreen - cannot be changed by the user
+        $RegKeyPathCSP = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP'
+
+    # To inject the initial wallpaper to the current user - can be changed by the user
+        $RegKeyPathUser = "HKCU:\$RegPathDesktopWallpaper"
+
+    # To inject the initial wallpaper for the ".Default" user profiles, so all new users never logged on gets it the first time. 
+        $RegKeyPathDefaultProfile = "HKU:\.DEFAULT\$RegPathDesktopWallpaper"
+
     # To inject the initial wallpaper to the last logged on user on the device - as system - can be changed by the user
-        $RegKeyPathUser = 'HKCU:\Control Panel\Desktop'
-    # To inject the initial wallpaper for the ".Default"  user profiles, so all new users never logged on gets it the first time. 
-        $RegKeyPatNewhUser = 'HKCU:\Control Panel\Desktop'
+        $RegKeyPathLastUser = "HKU:\$LastLoggedOnUserSID\$RegPathDesktopWallpaper"
+
         
 # PersonalizationCSP values
-    $DesktopPath = "DesktopImagePath"
-    $DesktopStatus = "DesktopImageStatus"
-    $DesktopUrl = "DesktopImageUrl"
-    $LockScreenPath = "LockScreenImagePath"
-    $LockScreenStatus = "LockScreenImageStatus"
-    $LockScreenUrl = "LockScreenImageUrl"
+    # Lockscreen CSP - HKLM
+        $LockScreenImageValue = "$LockScreenDestinationFile"
+        $LockScreenPath = "LockScreenImagePath"
+        $LockScreenStatus = "LockScreenImageStatus"
+        $LockScreenUrl = "LockScreenImageUrl"
 
-    $StatusValue = "1"
-    $DesktopImageValue = "$WallpaperDestinationFile"  
-    $LockScreenImageValue = "$LockScreenDestinationFile"
+    # Wallpaper CSP - HKLM
+        $DesktopImageValue = "$WallpaperDestinationFile"  
+        $DesktopPath = "DesktopImagePath"
+        $DesktopStatus = "DesktopImageStatus"
+        $DesktopUrl = "DesktopImageUrl"
+
+    # Status
+        $StatusValue = "1"
+
+    # wallpaper and paths normal user - HKCU/.Default
+        $WallpaperPath = "WallPaper"
+        $WallpaperPathValue = "$WallpaperDestinationFile"
+        $RegPath = "Control Panel\Desktop"
+
 
 # Starting configuration
+    # Lockscreen - cannot be changed by the user
+        if(!(Test-Path $RegKeyPathCSP))
+        {
+        New-Item -Path $RegKeyPathCSP -Force | Out-Null
+        New-ItemProperty -Path $RegKeyPathCSP -Name $LockScreenStatus -Value $StatusValue -PropertyType DWORD -Force | Out-Null
+        New-ItemProperty -Path $RegKeyPathCSP -Name $LockScreenPath -Value $LockScreenImageValue -PropertyType STRING -Force | Out-Null
+        New-ItemProperty -Path $RegKeyPathCSP -Name $LockScreenUrl -Value $LockScreenImageValue -PropertyType STRING -Force | Out-Null
+        } else {
+        New-ItemProperty -Path $RegKeyPathCSP -Name $LockScreenStatus -Value $Statusvalue -PropertyType DWORD -Force | Out-Null
+        New-ItemProperty -Path $RegKeyPathCSP -Name $LockScreenPath -Value $LockScreenImageValue -PropertyType STRING -Force | Out-Null
+        New-ItemProperty -Path $RegKeyPathCSP -Name $LockScreenUrl -Value $LockScreenImageValue -PropertyType STRING -Force | Out-Null
+        }
 
-IF(!(Test-Path $RegKeyPath))
+    # Wallpaper - cannot be changed by the user
+        if(!(Test-Path $RegKeyPathCSP))
+        {
+        New-Item -Path $RegKeyPathCSP -Force | Out-Null
+        New-ItemProperty -Path $RegKeyPathCSP -Name $DesktopStatus -Value $StatusValue -PropertyType DWORD -Force | Out-Null
+        New-ItemProperty -Path $RegKeyPathCSP -Name $DesktopPath -Value $DesktopImageValue -PropertyType STRING -Force | Out-Null
+        New-ItemProperty -Path $RegKeyPathCSP -Name $DesktopUrl -Value $DesktopImageValue -PropertyType STRING -Force | Out-Null
+        } else {
+        New-ItemProperty -Path $RegKeyPathCSP -Name $DesktopStatus -Value $Statusvalue -PropertyType DWORD -Force | Out-Null
+        New-ItemProperty -Path $RegKeyPathCSP -Name $DesktopPath -Value $DesktopImageValue -PropertyType STRING -Force | Out-Null
+        New-ItemProperty -Path $RegKeyPathCSP -Name $DesktopUrl -Value $DesktopImageValue -PropertyType STRING -Force | Out-Null
+        }
 
-{
+    # Wallpaper - for current user with standard rights        
+        if(!(Test-Path $RegKeyPathUser))
+        {
+        New-Item -Path $RegKeyPathUser -Force | Out-Null
+        New-ItemProperty -Path $RegKeyPathUser -Name $WallpaperPath -Value $WallpaperPathValue -PropertyType STRING -Force | Out-Null
+        } else {
+        New-ItemProperty -Path $RegKeyPathUser -Name $WallpaperPath -Value $WallpaperPathValue -PropertyType STRING -Force | Out-Null
+        }
 
-New-Item -Path $RegKeyPath -Force | Out-Null
+    # Section to set values via HKU as System user - to last logged on user on device and the .Default profile
 
-New-ItemProperty -Path $RegKeyPath -Name $DesktopStatus -Value $StatusValue -PropertyType DWORD -Force | Out-Null
-New-ItemProperty -Path $RegKeyPath -Name $LockScreenStatus -Value $StatusValue -PropertyType DWORD -Force | Out-Null
-New-ItemProperty -Path $RegKeyPath -Name $DesktopPath -Value $DesktopImageValue -PropertyType STRING -Force | Out-Null
-New-ItemProperty -Path $RegKeyPath -Name $DesktopUrl -Value $DesktopImageValue -PropertyType STRING -Force | Out-Null
-New-ItemProperty -Path $RegKeyPath -Name $LockScreenPath -Value $LockScreenImageValue -PropertyType STRING -Force | Out-Null
-New-ItemProperty -Path $RegKeyPath -Name $LockScreenUrl -Value $LockScreenImageValue -PropertyType STRING -Force | Out-Null
+        # Map HKU in registry as available drive in OS
+            New-PSDrive -PSProvider Registry -Name HKU -Root HKEY_USERS
 
-}
+        # Set the config
+            # Set the config values for the last logged on user
+                if((Test-Path -LiteralPath $RegKeyPathLastUser) -ne $true) {
+                    New-Item $RegKeyPathLastUser -force
+                        if ($?) {
+                        New-ItemProperty -LiteralPath "$RegKeyPathLastUser" -Name $WallpaperPath -Value $WallpaperPathValue -PropertyType String -Force -ea SilentlyContinue;
+                        } else {
+                            write-host "failed" -ForegroundColor Red
+                            }
+                    } else { 
+                    New-ItemProperty -LiteralPath "$RegKeyPathLastUser" -Name $WallpaperPath -Value $WallpaperPathValue -PropertyType String -Force -ea SilentlyContinue;
+                    }
 
-ELSE {
+            # Set the config values for the .Default profile
+                if((Test-Path -LiteralPath $RegKeyPathDefaultProfile) -ne $true) {
+                    New-Item $RegKeyPathDefaultProfile -force
+                        if ($?) {
+                        New-ItemProperty -LiteralPath "$RegKeyPathDefaultProfile" -Name $WallpaperPath -Value $WallpaperPathValue -PropertyType String -Force -ea SilentlyContinue;
+                        } else {
+                            write-host "failed" -ForegroundColor Red
+                            }
+                    } else { 
+                    New-ItemProperty -LiteralPath "$RegKeyPathDefaultProfile" -Name $WallpaperPath -Value $WallpaperPathValue -PropertyType String -Force -ea SilentlyContinue;
+                    }
 
-New-ItemProperty -Path $RegKeyPath -Name $DesktopStatus -Value $Statusvalue -PropertyType DWORD -Force | Out-Null
-New-ItemProperty -Path $RegKeyPath -Name $LockScreenStatus -Value $Statusvalue -PropertyType DWORD -Force | Out-Null
-New-ItemProperty -Path $RegKeyPath -Name $DesktopPath -Value $DesktopImageValue -PropertyType STRING -Force | Out-Null
-New-ItemProperty -Path $RegKeyPath -Name $DesktopUrl -Value $DesktopImageValue -PropertyType STRING -Force | Out-Null
-New-ItemProperty -Path $RegKeyPath -Name $LockScreenPath -Value $LockScreenImageValue -PropertyType STRING -Force | Out-Null
-New-ItemProperty -Path $RegKeyPath -Name $LockScreenUrl -Value $LockScreenImageValue -PropertyType STRING -Force | Out-Null
-}
-
+        # Unmap HKU from available PS drives before exiting
+                Remove-PSDrive hku
 
 # Restart explorer.exe
     stop-process -name explorer –force
