ec79ab4f90aa235e52618c21ffb34c5e423b8e47
Geir Dybbugt <59667456+geirdybbugt@users.noreply.github.com>
2023-01-20 09:34:11 +0100

    Upload

---
+++ Microsoft Bookings/ActivateMicrosoftBookingsWithMe.ps1
@@ -0,0 +1,28 @@
+﻿<#
+Microsoft Bookings With Me
+Create a new policy to activate the bookings with me feature for selected users.
+
+https://support.microsoft.com/en-gb/office/bookings-with-me-setup-and-sharing-ad2e28c4-4abd-45c7-9439-27a789d254a2
+#>
+
+# Connect to Exchange Online
+Connect-ExchangeOnline
+
+#Create a sharing policy to enable bookings with me
+$PolBookingsWithMe = "EnableBookWithMe"
+
+New-SharingPolicy -Name $PolBookingsWithMe -Domains Anonymous:CalendarSharingFreeBusySimple
+Set-SharingPolicy -Identity $PolBookingsWithMe -Domains @{Add="*:CalendarSharingFreeBusySimple"}
+
+# Add users to the policy
+$Users = @(
+    "user1@domain.com"
+    "user2@domain.com"
+)
+
+foreach ($user in $Users) {
+    Set-Mailbox -identity @user -SharingPolicy $PolBookingsWithMe
+}
+
+Remove-Variable PolBookingsWithMe
+Remove-Variable Users
