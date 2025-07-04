666eb6623b03b2270da4107c086af28a711e2fb8
Geir Dybbugt <geir@dybbugt.no>
2025-07-04 11:30:37 +0200

    Add Apple Bonjour Remediation Script

---
+++ Intune/Proactive Remediations/Remove Apple Bonjour/AppleBonjour_Detect.ps1
@@ -0,0 +1,16 @@
+﻿# Detect if bonjour is installed
+    $SearchPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall,HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
+
+    $TestForBonjour = Get-ChildItem $($SearchPath -split ",") -Recurse  | Get-ItemProperty | Where-Object {$_.DisplayName -Like "Bonjour" } | Select-Object -ExpandProperty Displayname
+    $result = $TestForBonjour -match "Bonjour"
+
+    if ($result)
+     {
+        Write-Output "Bonjour installed, remediation needed"
+        exit 1
+     }
+     else
+     {
+        Write-Output "Bonjour missing, no change needed"
+        exit 0
+     }
---
+++ Intune/Proactive Remediations/Remove Apple Bonjour/AppleBonjour_Remediate.ps1
@@ -0,0 +1,10 @@
+﻿# Remediate - uninstall bonjour if found to be installed
+
+$app = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -contains "Bonjour" }
+
+
+ if ($app -ne $null) {
+	$app.Uninstall()
+} else {
+	Write-Output "Application not found."
+}
